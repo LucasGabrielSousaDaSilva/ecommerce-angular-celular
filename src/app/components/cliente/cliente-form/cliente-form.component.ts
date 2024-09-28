1bf18e62ff2fa8fa49b52b6bc6cab94a3a209fd0
LucasGabrielSousaDaSilva <gabrielsousa@unitins.br>
2024-09-28 01:13:41 -0300

    typeScript do cliente foi adicionada, mas se encontra com alguns problemas

--- src/app/components/cliente/cliente-form/cliente-form.component.ts
+++ src/app/components/cliente/cliente-form/cliente-form.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ClienteService } from '../../../services/cliente.service';
+import { Router } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-cliente-form',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, NgIf, MatInputModule, ReactiveFormsModule],
   templateUrl: './cliente-form.component.html',
   styleUrl: './cliente-form.component.css'
 })
 export class ClienteFormComponent {
+  formGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,
+    private clienteService: ClienteService,
+    private router: Router) {
+      this.formGroup = this.formBuilder.group({
+        nome:['', Validators.required],
+        cep:['', Validators.required],
+      }) 
+  }
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      const novoCliente = this.formGroup.value;
+      this.clienteService.insert(novoCliente).subscribe({
+        next: (estadoCadastrado) => {
+          this.router.navigateByUrl('/estados');
+        },
+        error: (err) => {
+          console.log('Erro ao salvar', + JSON.stringify(err));
+        }
+      })
+    }
+  }
 }
--- src/app/components/cliente/cliente-list/cliente-list.component.ts
+++ src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from "../../../models/cliente.model";
+import { ClienteService } from '../../../services/cliente.service';
+import { NgFor } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatLabel } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatCardActions } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatCardContent } from '@angular/material/card';
+import { MatCardTitle } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-cliente-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, RouterModule, MatCardActions,
+    MatLabel, MatFormField, MatCardContent, MatCardTitle, CommonModule, MatCardModule, ReactiveFormsModule,
+    MatInputModule],
   templateUrl: './cliente-list.component.html',
   styleUrl: './cliente-list.component.css'
 })
-export class ClienteListComponent {
+export class ClienteListComponent implements OnInit{
+  
+  clientes: Cliente[] = [];
+  displayerColemns: string[] = ['id', 'nome', 'cpf', 'cep', 'acao'];
+
+  clienteForm!: FormGroup;
+  clienteSelecionado: Cliente | null = null;
+
+    constructor(private clienteService: ClienteService){}
+ 
+ 
+  ngOnInit(): void {
+    this.clienteService.findAll().subscribe(data => {
+      this.clientes = data;
+    });
+  }
+
+  editarCliente(cliente: Cliente): void {
+    this.clienteSelecionado = cliente;
+
+    // Preencher o formulário com os dados do funcionário selecionado
+    this.clienteForm.patchValue({
+      nome: cliente.nome,
+      cep: cliente.cep,
+      cpf: cliente.cpf,
+    });
+  }
+
+  salvarcliente(): void {
+    if (this.clienteForm.valid && this.clienteSelecionado) {
+      const clienteEditado = {
+        ...this.clienteSelecionado,
+        ...this.clienteForm.value
+      };
+
+      // Aqui, envie os dados editados para o serviço
+      this.clienteService.update(clienteEditado).subscribe(() => {
+        this.ngOnInit();  // Atualizar a lista após a edição
+        this.cancelarEdicao();        // Fechar o card de edição
+      });
+    }
+  }
+
+  cancelarEdicao(): void {
+    this.clienteSelecionado = null;
+    this.clienteForm.reset();  // Reseta o formulário
+  }
 
 }
