5026b74e9da8eddb0f7f4e2d89ad20345f609335
LucasGabrielSousaDaSilva <gabrielsousa@unitins.br>
2024-09-28 01:12:18 -0300

    Colocando o css e html com seus caminhos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { clienteResolver } from "./components/cliente/resolver/cliente.resolver";
+import { ClienteFormComponent } from './components/cliente/cliente-form/cliente-form.component';
+import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'clientes/edit/:id', component: ClienteFormComponent, resolve:{Cliente: clienteResolver}},
+    {path: 'clientes', component: ClienteListComponent, title: 'Lista de Clientes'},
+    {path: 'clientes/new', component: ClienteFormComponent, title: 'Novos Clientes'}
+];
--- src/app/components/cliente/resolver/cliente.resolver.ts
+++ src/app/components/cliente/resolver/cliente.resolver.ts
@@ -1,5 +1,8 @@
-import { ResolveFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Cliente } from "../../../models/cliente.model";
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ClienteService } from '../../../services/cliente.service';
 
-export const clienteResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
+export const clienteResolver: ResolveFn<Cliente> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  return inject(ClienteService).findById(route.paramMap.get('id')!);
 };
