4695404d7189643b3270c98593732d29466a9506
Julio Cesar <juliocesararraes42@gmail.com>
2024-10-25 21:04:43 -0300

    Adiciona modelos

---
+++ src/app/models/funcionario.model.ts
@@ -0,0 +1,9 @@
+export class Funcionario {
+    id!: number;
+    nome!: string;
+    cep!: string;
+    cpf!: string;
+    cnpj!: string;
+    login!: string;
+    senha!: string;
+}
---
+++ src/app/models/porta-slot.model.ts
@@ -0,0 +1,4 @@
+export class PortaSlot {
+    id! : number;
+    tipo! : string;
+}
